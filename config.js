c0579d09d5ad0f22fb0cb2c67c1b2493890b2dca
Iliavsevolodov <78146096+Iliavsevolodov@users.noreply.github.com>
2026-09-22 10:07:34 +0300

    Add Supabase client configuration

---
+++ config.js
@@ -0,0 +1,5 @@
+window.RECOGNITION_CONFIG = {
+  SUPABASE_URL: "https://uifmhboadvocmvkxmimi.supabase.co",
+  SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVpZm1oYm9hZHZvY212a3htaW1pIiwicm9sZSI6ImFub24iLCJpYXQiOjE3OTAwMTU4ODEsImV4cCI6MjEwNTU5MTg4MX0.6LhXYtt3uAZEfKyPODrYL_MGt7OewCHLQCuPRiu_pq8",
+  EVENT_TITLE: "Признание на сцене"
+};
